ef42fc706135360a39980dd83345a71ecff38d73
Riad_Hjm <riad.elhajjame@uir.ac.ma>
2023-10-29 14:30:30 +0100

    Banner.js
    Created a banner when you click on the contact us button

---
+++ src/Components/Banner.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+import Navbar from "./Navbar";
+
+const Banner = ({ onClose }) => {
+    return (
+
+        <div id="newsletter-banner" tabindex="-1" class="fixed top-0 left-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+            <div class="flex items-center flex-shrink-0 w-full mx-auto sm:w-auto">
+                <form action="#" class="flex flex-col items-center w-full md:flex-row">
+                    <label for="text" class="flex-shrink-0 mb-2 mr-auto text-sm font-medium text-gray-500 md:mb-0 md:mr-4 dark:text-gray-400 md:m-0">Send us your message here</label>
+                    <input type="text" id="email" autocomplete="off" placeholder="Enter your message here.." class="bg-white border border-gray-300 text-gray-900 md:w-64 mb-2 md:mb-0 md:mr-4 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    <button type="submit" class="text-white bg-midnight-blue-900 hover:bg-midnight-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+                </form>
+            </div>
+            <div class="flex items-center absolute top-2.5 right-2.5 md:relative md:top-auto md:right-auto">
+                <button onClick={onClose} data-dismiss-target="#newsletter-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close banner</span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import logoUir from '../assets/uir-logo.png';
 import { Link } from 'react-router-dom';
+import Banner from "./Banner";
 
 const Navbar = () => {
+    const [showBanner, setShowBanner] = useState(false);
+
+    function handleBannerClick()
+    {
+        setShowBanner(true);
+    }
+    const handleBannerClose = () => 
+    {
+        setShowBanner(false);
+      };
+
     return (
         <nav className="bg-midnight-blue-900 dark:bg-midnight-blue-900 fixed w-full z-20 top-0 left-0 border-b border-midnight-blue-600 dark:border-barberry-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -10,7 +22,9 @@ const Navbar = () => {
                     <img src={logoUir} className="h-14 mr-3" alt="UIR Logo" />
                 </Link>
                 <div className="flex md:order-2">
-                    <button type="button" className="text-midnight-blue-900 bg-barberry-600 duration-300 hover:text-midnight-blue-700 hover:bg-barberry-700 focus:ring-4 focus:outline-none focus:text-white font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-barberry-600 dark:hover:bg-barberry-700 dark:focus:text-white">Contact Us</button>
+                    <button type="button" 
+                            onClick={showBanner ? handleBannerClose : handleBannerClick}
+                            className="text-midnight-blue-900 bg-barberry-600 duration-300 hover:text-midnight-blue-700 hover:bg-barberry-700 focus:ring-4 focus:outline-none focus:text-white font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-barberry-600 dark:hover:bg-barberry-700 dark:focus:text-white">Contact Us</button>
                     <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-barberry-600 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -35,6 +49,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
+            {showBanner && <Banner onClose={handleBannerClose} />}
         </nav>
     )
 }
