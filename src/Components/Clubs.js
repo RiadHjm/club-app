c5678262d82575da36c23e9729e55b2a86af833f
Riad_Hjm <riad.elhajjame@uir.ac.ma>
2023-11-26 21:36:59 +0100

    Pagination + Search button

--- src/Components/Clubs.js
+++ src/Components/Clubs.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Loading from './Loading';
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 type CommitteeMember = {
     idA: number;
@@ -59,6 +60,19 @@ const Clubs = () => {
     const [pageSize, setPageSize] = useState(10);
     const [page, setPage] = useState(0);
     const [maxPages, setMaxPages] = useState(1);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handlePreviousPage = () => {
+        if (pageNumber > 0) {
+          setPageNumber(pageNumber - 1);
+        }
+      };
+      
+      const handleNextPage = () => {
+        if (pageNumber < maxPages - 1) {
+          setPageNumber(pageNumber + 1);
+        }
+      };
 
     const fetchClubs = async () => {
         try {
@@ -66,6 +80,7 @@ const Clubs = () => {
                 params: {
                     pageNumber: pageNumber,
                     pageSize: pageSize,
+                    search: searchTerm,
                 },
                 headers: {
                     'Content-Type': 'application/json',
@@ -83,11 +98,18 @@ const Clubs = () => {
     useEffect(() => {
         fetchClubs();
     }, [pageNumber, pageSize])
+
+
     return (
         <div className='flex flex-col mt-[150px] mb-24 gap-20'>
 
             <div className='flex justify-end'>
-                <form className='w-[80vh]'>
+                <form className='w-[80vh]'
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        fetchClubs();
+                    }}
+                >
                     <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                     <div class="relative">
                         <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -95,7 +117,7 @@ const Clubs = () => {
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Clubs . . ." />
+                        <input type="search" id="default-search" autoComplete='off' class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Clubs . . ." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
                         <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-midnight-blue-900 hover:bg-barberry-600 duration-300 focus:ring-4 focus:outline-none focus:ring-barberry-500 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                     </div>
                 </form>
@@ -117,9 +139,15 @@ const Clubs = () => {
                     </div>
                 )}
                 <div>
-                    <p>Page Number: {pageNumber}</p>
+                    <p>Page Number: {pageNumber + 1}</p>
                     <p>Page Size: {pageSize}</p>
                 </div>
+                
+                <div className="flex justify-center gap-44 mt-4">
+                    <button onClick={handlePreviousPage} disabled={pageNumber === 0} className='text-white hover:text-[#D0D700] ease-in-out duration-300 bg-midnight-blue-900 hover:bg-midnight-blue-800 focus:outline-none font-medium rounded-full text-sm px-10 py-2.5 text-center mr-2 mb-2 dark:bg-midnight-blue-800 dark:hover:bg-midnight-blue-900'>Previous</button>
+                    <button onClick={handleNextPage} disabled={pageNumber === maxPages - 1} className='text-white hover:text-[#D0D700] ease-in-out duration-300 bg-midnight-blue-900 hover:bg-midnight-blue-800 focus:outline-none font-medium rounded-full text-sm px-10 py-2.5 text-center mr-2 mb-2 dark:bg-midnight-blue-800 dark:hover:bg-midnight-blue-900'>Next</button>
+                </div>
+
             </div>
         </div>
     )
